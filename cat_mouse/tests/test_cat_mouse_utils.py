ec89f809f439fee76fad53da52cbb27c711de050
Chanin Maiprom <6210110088@psu.ac.th>
2022-09-30 20:29:15 +0700

    cat_mouse test4

--- cat_mouse/tests/test_cat_mouse_utils.py
+++ cat_mouse/tests/test_cat_mouse_utils.py
@@ -34,3 +34,35 @@ class CatandMouseTest(unittest.TestCase):
         expected_output = None
         result = cat_mouse(var_x, var_y, var_z)
         self.assertIsNone(result, expected_output)
+
+    def test_give_0_5_6_should_be_None(self):
+        var_x = 0
+        var_y = 5
+        var_z = 6
+        expected_output = None
+        result = cat_mouse(var_x, var_y, var_z)
+        self.assertIsNone(result, expected_output)
+
+    def test_give_33_86_59_should_be_CatA(self):
+        var_x = 33
+        var_y = 86
+        var_z = 59
+        expected_output = 'Cat A'
+        result = cat_mouse(var_x, var_y, var_z)
+        self.assertEqual(result, expected_output)
+
+    def test_give_22_75_70_should_be_CatB(self):
+        var_x = 22
+        var_y = 75
+        var_z = 70
+        expected_output = 'Cat B'
+        result = cat_mouse(var_x, var_y, var_z)
+        self.assertEqual(result, expected_output)
+
+    def test_give_21_95_58_should_be_MouseC(self):
+        var_x = 21
+        var_y = 95
+        var_z = 58
+        expected_output = 'Mouse C'
+        result = cat_mouse(var_x, var_y, var_z)
+        self.assertEqual(result, expected_output)
