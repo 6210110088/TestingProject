c0254487ad5edb14ff08ddfa3f0bdf19579dec9d
Chanin Maiprom <6210110088@psu.ac.th>
2022-09-30 14:38:47 +0700

    caesarcipher test3

--- caesarcipher/tests/test_caesarcipher_utils.py
+++ caesarcipher/tests/test_caesarcipher_utils.py
@@ -9,3 +9,17 @@ class CaesarCipherTest(unittest.TestCase):
         expected_result = 'okffng-Qwvb'
         result = caesarCipher(text, rotate)
         self.assertEqual(result, expected_result)
+
+    def test_give_AlwaysLookontheBrightSideofLife_5_is_FqbfdxQttptsymjGwnlmyXnijtkQnkj(self):
+        text = 'Always-Look-on-the-Bright-Side-of-Life'
+        rotate = 5
+        expected_result = 'Fqbfdx-Qttp-ts-ymj-Gwnlmy-Xnij-tk-Qnkj'
+        result = caesarCipher(text, rotate)
+        self.assertEqual(result, expected_result)
+        
+    def test_give_Theresastarmanwaitinginthesky_3_is_Wkhuhvdvwdupdqzdlwlqjlqwkhvnb(self):
+        text = 'There\'s-a-starman-waiting-in-the-sky'
+        rotate = 5
+        expected_result = 'Wkhuh\'v-d-vwdupdq-zdlwlqj-lq-wkh-vnb'
+        result = caesarCipher(text, rotate)
+        self.assertEqual(result, expected_result)
