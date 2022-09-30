04799e8cb4fc008066960b29ac4e4b58933c2873
Chanin Maiprom <6210110088@psu.ac.th>
2022-09-30 17:35:34 +0700

    twochar test2

--- twochar/tests/test_twochar_utils.py
+++ twochar/tests/test_twochar_utils.py
@@ -7,3 +7,33 @@ class TwoCharTest(unittest.TestCase):
         expected_result = 5
         result = alternate(text)
         self.assertEqual(result, expected_result)
+
+    def test_give_asdcbsdcagfsdbgdfanfghbsfdab_is_8(self):
+        text = 'asdcbsdcagfsdbgdfanfghbsfdab'
+        expected_result = 8
+        result = alternate(text)
+        self.assertEqual(result, expected_result)
+
+    def test_give_asvkugfiugsalddlasguifgukvsa_is_0(self):
+        text = 'asvkugfiugsalddlasguifgukvsa'
+        expected_result = 0
+        result = alternate(text)
+        self.assertEqual(result, expected_result)
+
+    def test_give_a_is_0(self):
+        text = 'a'
+        expected_result = 0
+        result = alternate(text)
+        self.assertEqual(result, expected_result)
+
+    def test_give_cwomzxmuelmangtosqkgfdqvkzdnxerhravxndvomhbokqmvsfcaddgxgwtpgpqrmeoxvkkjunkbjeyteccpugbkvhljxsshpoymkryydtmfhaogepvbwmypeiqumcibjskmsrpllgbvc_is_8(self):
+        text = 'cwomzxmuelmangtosqkgfdqvkzdnxerhravxndvomhbokqmvsfcaddgxgwtpgpqrmeoxvkkjunkbjeyteccpugbkvhljxsshpoymkryydtmfhaogepvbwmypeiqumcibjskmsrpllgbvc'
+        expected_result = 8
+        result = alternate(text)
+        self.assertEqual(result, expected_result)
+
+    def test_give_ab_is_2(self):
+        text = 'ab'
+        expected_result = 2
+        result = alternate(text)
+        self.assertEqual(result, expected_result)
