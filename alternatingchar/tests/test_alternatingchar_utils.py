1abd0863085295343d33b10ed28f2e8499d8c3e4
Chanin Maiprom <6210110088@psu.ac.th>
2022-09-30 13:57:59 +0700

    alternatingchar test2

--- alternatingchar/tests/test_alternatingchar_utils.py
+++ alternatingchar/tests/test_alternatingchar_utils.py
@@ -7,3 +7,27 @@ class AlternatingCharTest(unittest.TestCase):
         expected_result = 3
         result = alternatingCharacters(text)
         self.assertEqual(result, expected_result)
+
+    def test_give_bbbbb_is_4(self):
+        text = 'bbbbb'
+        expected_result = 4
+        result = alternatingCharacters(text)
+        self.assertEqual(result, expected_result)
+
+    def test_give_abababab_is_0(self):
+        text = 'abababab'
+        expected_result = 0
+        result = alternatingCharacters(text)
+        self.assertEqual(result, expected_result)
+
+    def test_give_bababa_is_0(self):
+        text = 'bababa'
+        expected_result = 0
+        result = alternatingCharacters(text)
+        self.assertEqual(result, expected_result)
+
+    def test_give_aaabbb_is_4(self):
+        text = 'aaabbb'
+        expected_result = 4
+        result = alternatingCharacters(text)
+        self.assertEqual(result, expected_result)
