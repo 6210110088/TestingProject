39776ab0c8d1b920910d6fb736ace260de7a2efe
Chanin Maiprom <6210110088@psu.ac.th>
2022-09-30 20:11:06 +0700

    staircase test2

--- staircase/tests/test_staircase_utils.py
+++ staircase/tests/test_staircase_utils.py
@@ -8,3 +8,17 @@ class StairCaseTest(unittest.TestCase):
         expected_output = [' #', '##']
         result = staircase(n, pattern)
         self.assertEqual(result, expected_output)
+
+    def test_give_4_with_star_should_be_starstarstarstar(self):
+        n = 4
+        pattern = '*'
+        expected_output = ['   *', '  **', ' ***', '****']
+        result = staircase(n, pattern)
+        self.assertEqual(result, expected_output)
+
+    def test_give_1_with_a_should_be_a(self):
+        n = 1
+        pattern = 'a'
+        expected_output = ['a']
+        result = staircase(n, pattern)
+        self.assertEqual(result, expected_output)
