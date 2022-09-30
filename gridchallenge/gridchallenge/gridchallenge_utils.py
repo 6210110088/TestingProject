606615d1ba6f54139fb53914d755ef70f5b3f2df
Chanin Maiprom <6210110088@psu.ac.th>
2022-09-30 19:05:52 +0700

    gridchallenge

---
+++ gridchallenge/gridchallenge/gridchallenge_utils.py
@@ -0,0 +1,23 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridChallenge' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING_ARRAY grid as parameter.
+#
+
+def gridChallenge(grid):
+    # Write your code here
+    for i in range(len(grid)):
+        grid[i] = sorted(grid[i])
+        
+    for j in range(len(grid[0])):
+        for i in range(1,len(grid)):
+            if grid[i][j]<grid[i-1][j]:
+                return "NO"
+    return "YES"
